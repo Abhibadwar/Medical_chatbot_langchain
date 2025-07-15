2836ffad6c629678fddf6ab753a35bd01472c8d5
Abhishek Badwar <98725240+Abhibadwar@users.noreply.github.com>
2025-07-15 12:29:57 +0530

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='MedicalCatBot',
+    version='0.1',
+    author='Abhishek Badwar',
+    author_email='abhibadwar0724@gmail.com',
+    packages=find_packages(),
+    install_requires=[]
+)
